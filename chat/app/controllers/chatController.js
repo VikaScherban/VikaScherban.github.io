86593c4c3aac8c2c3f5ced435b87bc73a7a3a87a
VikaScherban <www.viki.wwww@gmail.com>
2018-02-18 17:52:49 +0200

    first version of chat

---
+++ chat/app/controllers/chatController.js
@@ -0,0 +1,60 @@
+/**
+ * Created by stranger on 18.02.18.
+ */
+
+'use strict';
+
+angular.module("myApp", ['ngRoute','pubnub.angular.service'])
+    .controller('ChatCtrl', function($scope, Pubnub) {
+        $scope.channel = 'messages-channel';""
+        // Generating a random uuid between 1 and 10000 using an utility function from the lodash library.
+        $scope.uuid = Math.floor(Math.random() * 10000).toString();
+        Pubnub.init({
+            publish_key: 'pub-c-92d96d75-2ae0-4233-aa40-d9767530b195',
+            subscribe_key: 'sub-c-7fb592b8-14b8-11e8-92ea-7a3d09c63f1f',
+            uuid: $scope.uuid
+        });
+
+        // Send the messages over PubNub Network
+        $scope.sendMessage = function() {
+            // Don't send an empty message
+            if (!$scope.messageContent || $scope.messageContent === '') {
+                return;
+            }
+            Pubnub.publish({
+                channel: $scope.channel,
+                message: {
+                    content: $scope.messageContent,
+                    sender_uuid: $scope.uuid,
+                    date: new Date()
+                },
+                callback: function(m) {
+                    console.log(m);
+                }
+            });
+            // Reset the messageContent input
+            $scope.messageContent = '';
+
+        };
+
+        $scope.messages = [];
+
+// Subscribing to the ‘messages-channel’ and trigering the message callback
+        Pubnub.subscribe({
+            channel: $scope.channel,
+            triggerEvents: ['callback']
+        });
+
+// Listening to the callbacks
+        $scope.$on(Pubnub.getMessageEventNameFor($scope.channel), function (ngEvent, m) {
+            $scope.$apply(function () {
+                $scope.messages.push(m)
+            });
+        });
+
+// A function to display a nice uniq robot avatar
+        $scope.avatarUrl = function(uuid){
+            return 'http://robohash.org/'+uuid+'?set=set2&bgset=bg2&size=70x70';
+        };
+
+    });
